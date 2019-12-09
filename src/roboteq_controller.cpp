1275cac1fa3273ceaa0cb6552236a57a9f40ab2c
akeaveny <akeaveny@uwaterloo.ca>
2019-12-09 14:26:52 -0500

    example program

--- src/roboteq_controller.cpp
+++ src/roboteq_controller.cpp
@@ -8,9 +8,4 @@ namespace roboteq {
      roboteq_controller::roboteq_controller(std::unique_ptr<i_comm> &&comm) : _comm(std::move(comm)) {
     }
 
-    void Roboteq::SetPosition(long _position, uint8_t _channel)
-        {
-            i_comm::download(runtime_command command, uint8_t subindex, uint32_t data);
-        }
-
 } // namespace roboteq
